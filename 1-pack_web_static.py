a926cf6ed9d85b455aec5d2acedbeae2ab9d018d
Peter Chibunna <peterchibunna@gmail.com>
2023-01-23 22:36:16 +0100

    TAsk 01, modified

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ from fabric.api import local
 def do_pack():
     """Creates a tar gun-zipped archive of the `web_static` directory"""
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+    file = "versions/web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz".format(
         dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
