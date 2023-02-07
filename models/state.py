a8a647e60a695640a7fd3b627967271ec357dafd
Peter Chibunna <peterchibunna@gmail.com>
2023-02-07 19:39:48 +0100

    debugging task #7, 1 checkers failing

--- models/state.py
+++ models/state.py
@@ -10,13 +10,13 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    # if getenv("HBNB_TYPE_STORAGE") == "db":
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
-    # else:
-    #     name = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",  backref="state", cascade="delete")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    # if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """Get a list of all related City objects."""
