06774302f15978f4f2cc0653e2cb2c7a41b5fc90
Peter Chibunna <peterchibunna@gmail.com>
2023-02-07 21:10:08 +0100

    debugging task #7, 1-checker module doc issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 """ Start Flask web app
 The application listens on 0.0.0.0, on the default port: 5000.
 Routes:
     /states_list: Shows the states in the db?
-    """
+"""
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 
 app = Flask(__name__)
