18f236c8afae0968daae5601eb5ed497e8c9f8e1
Peter Chibunna <peterchibunna@gmail.com>
2023-02-07 11:49:36 +0100

    change port

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -58,4 +58,4 @@ def hello_hbnb6(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -71,4 +71,4 @@ def hello_hbnb7(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
