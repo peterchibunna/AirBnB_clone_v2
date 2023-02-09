f665bcf5a1c0ab45f7c381601a2344d54798f343
Peter Chibunna <peterchibunna@gmail.com>
2023-02-09 14:58:49 +0100

    Task #8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-Start Flask web app
-The application listens on 0.0.0.0, on the default port: 5000.
-Routes:
-    /states_list: Shows the states in the db?
-"""
-
-from flask import Flask, render_template
+""" Start Flask Web Application """
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def list_states():
+def states_list():
     """ return list of State Objects """
-    data = storage.all(State)
-    return render_template('7-states_list.html', states=data)
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_all(ctx):
-    """ removes the current Session """
+def teardown(exc):
+    """ remove the current Session  """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
