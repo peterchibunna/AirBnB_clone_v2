08e868ed8e611e7d01d18848fcfe9b8be9ce1e1b
Peter Chibunna <peterchibunna@gmail.com>
2023-03-09 14:55:23 +0100

    Test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -14,7 +14,6 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
-
     States are sorted by name.
     """
     states = storage.all("State")
